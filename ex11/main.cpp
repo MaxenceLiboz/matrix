eb77debe17833c533a757ae664936fc070720a9c
Maxence Liboz <mliboz@student.42lyon.fr>
2024-06-07 11:37:12 +0200

    Feat: ex11, add some examples to the main

--- ex11/main.cpp
+++ ex11/main.cpp
@@ -20,6 +20,14 @@ void run_determinant() {
     Matrix<float> m4 = Matrix<float>({{8, 5, -2, 4}, {4, 2.5, 20, 4}, {8, 5, 1 ,4}, {28, -4, 17, 1}});
     std::cout << "m4: " << std::endl << m4;
     std::cout << "Determinant: " << m4.determinant() << std::endl;
+
+    Matrix<float> m5 = Matrix<float>({{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}});
+    std::cout << "m5: " << std::endl << m5;
+    std::cout << "Determinant: " << m5.determinant() << std::endl;
+
+    Matrix<float> m6 = Matrix<float>({{1, 2, 3}, {4, 5, 6}});
+    std::cout << "m6: " << std::endl << m6;
+    std::cout << "Determinant: " << m6.determinant() << std::endl;
 }
 
 
